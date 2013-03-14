b08f63085e7bb9b648142b24ce79f0679d6dac63
Samu Juvonen <samu.juvonen@gmail.com>
2013-03-14 18:37:18 +0200

    Add ZF2 Module class

---
+++ Module.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Samu;
+
+class Module {
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    'Samu\Widget\Table' => __DIR__ . '/src/Samu/Widget/Table',
+                    'Samu\Zend\Table' => __DIR__ . '/src/Samu/Zend/Table',
+                ),
+            ),
+        );
+    }
+
+    public function getConfig() {
+        return array(
+            'view_helpers' => array(
+                'invokables' => array(
+                    'SamuTable' => 'Samu\Zend\Table\View\Helper\Table',
+                ),
+            ),
+        );
+    }
+}
