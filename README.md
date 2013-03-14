ed1d6cabc95d09f8a556d1c8b240c5c9fe911b0e
Samu Juvonen <samu.juvonen@gmail.com>
2013-03-14 19:20:46 +0200

    Update docs

--- README.md
+++ README.md
@@ -4,38 +4,8 @@ table-widget
 Table widget for generating rich and customizable tables from datasets easily. Also featured is a view helper for integrating the table into Zend Framework 2 (ZF2).
 
 Instructions for ZF2 integration:
+    1. Install this module (SamuTable)
+    2. Enable it in your application.config.php
 
-I suppose the easiest way is to copy directory ./src/Samu/ under any ZF2 module in your Zend project.
-For example "zendroot/module/MyApp/src/Samu/". After that it is enough to add namespace Samu into
-your module's autoloader configuration and also define the view helper in module.config.php.
-
-Example Module::getAutoloaderConfig():
-
-    public function getAutoloaderConfig() {
-            return array(
-                    'Zend\Loader\ClassMapAutoloader' => array(
-                            array(),
-                    ),
-                    'Zend\Loader\StandardAutoloader' => array(
-                            'namespaces' => array(
-                                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-
-                    // Simply add this row:
-                                    'Samu' => __DIR__ . '/src/Samu',
-                            ),
-                    ),
-            );
-    }
-
-module.config.php:
-
-    return array(
-        'view_helpers' => array(
-            'invokables' => array(
-                'Table' => 'Samu\Zend\Table',
-            ),
-        ),
-    );
-
-Then in your view template you can access the view helper by simply calling $this->table().
+Then in your view template you can access the view helper by simply calling $this->samuTable().
 
---
+++ docs/zf2_hydration.php
@@ -0,0 +1,47 @@
+## Using hydrators with the ZF2 table view helper
+
+Hydrators are quite nice when the items to be displayed are 'real' objects with private or protected members instead of simple stdClass objects or plain arrays. Hydrators map the objects into arrays that can be then used with the table widget. The ZF2 plugin for Table widget comes with built-in support for hydrators.
+
+For basic information about using hydrators with objects, read this article: https://github.com/doctrine/DoctrineModule/blob/master/docs/hydrator.md
+
+### Initialing the table with a hydrator
+An easy way to setup the table widget with a hydrator is to set a hydrator factory somewhere in the initialiation phase of your ZF2 app. Any callable item can be a hydrator factory. A single parameter will be passed to the callable: FQCN of the class for which a hydrator is being created.
+
+Here's how to set the factory in onBootstrap method of your central module:
+
+```php
+namespace MyModule;
+
+// Will extract properties using public API of the object
+use Zend\Stdlib\Hydrator\ClassMethods as ZendHydrator;
+
+class Module {
+    public function onBootstrap($e) {
+        \Samu\Zend\Table\Table::setHydratorFactory(function($class) {
+            // Use the same hydrator for all classes
+            return ZendHydrator();
+        });
+    }
+}
+```
+
+Here's an extended example demonstrating using hydrators with Doctrine (requires DoctrineModule):
+
+```php
+namespace MyModule;
+
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+
+class Module {
+    public function onBootstrap($e) {
+        $sm = $e->getApplication()->getServiceManager();
+
+        \Samu\Zend\Table\Table::setHydratorFactory(function($class) use ($sm) {
+            $em = $sm->get('Doctrine\ORM\EntityManager');
+            $hydrator = new DoctrineHydrator($em, $class);
+            return $hydrator;
+        });
+    }
+}
+
+**NOTE** The factory will be called only once for each instance (of the table)! The class name that is passed to the hydrator is deduced from the first row of the result set. This is important to remember when the result is not homogenous.
